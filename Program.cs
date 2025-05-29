dad8a05b480ed2f6f2ab04e71ef465186ab73ccf
ArdillaV21 <fedevalle21@gmail.com>
2025-05-29 05:00:46 -0300

    Lectura de archivos y creacion del archivo csv

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-
+﻿
 
 string? path;
 do{
@@ -11,3 +10,34 @@ do{
 }while(!Directory.Exists(path));
 
 Console.WriteLine("Carpetas: \n");
+string[] carpetas = Directory.GetDirectories(path);
+
+Console.WriteLine("Archivos: \n");
+string[] archivos = Directory.GetFiles(path);
+
+foreach (var ar in archivos)
+{
+    FileInfo info = new FileInfo(ar);
+    long tamaño = info.Length; //Bytes
+    Console.WriteLine(Path.GetFileName(ar)+" tamaño: " + tamaño);
+}
+
+string ruta = "Archivos/reporte_archivos.csv";
+FileStream fs = new FileStream(ruta,FileMode.Create);
+using (StreamWriter str = new StreamWriter(fs)){
+    str.WriteLine("Nombre,Tamaño");
+    foreach (var ar in archivos)
+    {
+        string nombre = Path.GetFileName(ar);
+        FileInfo info = new FileInfo(ar);
+        long tamano = info.Length; //Bytes
+        str.WriteLine($"{nombre},{tamano}");
+    }
+    foreach (var car in carpetas)
+    {
+        string nombre = Path.GetFileName(car);
+        str.WriteLine("{0}",nombre);
+    }
+    str.Close();
+}
+
--- obj/Debug/net8.0/LectorDirectorio.AssemblyInfo.cs
+++ obj/Debug/net8.0/LectorDirectorio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LectorDirectorio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f9bc497bb127e8224a5729d6a3994cdd1c3dda5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ef4164f8e439006f09724620ba8355829bd978d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LectorDirectorio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LectorDirectorio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
